af15be61a0188da1906fd62e0670bd5770c30e0e
Bert Van Nuffelen <Bert.vannuffelen@gmail.com>
2016-10-26 11:53:32 +0000

    remove any memory setting

--- config/tomcat-setenv.sh
+++ config/tomcat-setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 CLASSPATH="$CLASSPATH:/deployment"
 
-JAVA_OPTS="${JAVA_OPTS} -Xms512m"
+JAVA_OPTS="${JAVA_OPTS}"
 
 # use this configuration instead of creating the .aduna files
 # -Dinfo.aduna.platform.appdata.basedir=/var/lib/tomcat6/data
