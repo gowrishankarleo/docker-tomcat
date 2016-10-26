57aa638f40a293cdf6f7159e3405e1b0710a26e9
Bert Van Nuffelen <Bert.vannuffelen@gmail.com>
2016-10-26 11:33:13 +0000

    remove max memory limit

--- config/tomcat-setenv.sh
+++ config/tomcat-setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 CLASSPATH="$CLASSPATH:/deployment"
 
-JAVA_OPTS="${JAVA_OPTS} -Xmx1g -Xms512m"
+JAVA_OPTS="${JAVA_OPTS} -Xms512m"
 
 # use this configuration instead of creating the .aduna files
 # -Dinfo.aduna.platform.appdata.basedir=/var/lib/tomcat6/data
